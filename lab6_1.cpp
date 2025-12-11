3305c2d90e301003ef40c4894c9c241dadf0292a
NattananKat <natkat.nattanan@gmail.com>
2025-12-11 20:30:19 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    int i = 0;
+    int N = 1;
+    int even = 0;
+    int odd = 0;
+
+    while(N != 0){
+        cout << "Enter an integer: ";
+        cin >> N;
+        i++;
+
+        if(N % 2 == 0){
+        even++;
+        }else{
+        odd++;
+        }
+    }
+    
+    cout << "#Even numbers = " << even-1 << "\n";
+    cout << "#Odd numbers = " << odd << "\n";
 }
